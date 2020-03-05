9f9e28fc5e4d5d51acfa4ce4ceab28726a68acd5
Amir Omidi <amir@aaomidi.com>
2020-03-05 14:13:54 -0800

    Update to ts

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import { Toolkit } from 'actions-toolkit';
+
+
+interface LabelDefinition {
+    id: number,
+    name: string
+}
+
+interface RepoContext {
+    repo: string,
+    owner: string
+}
+
+const tools = new Toolkit({
+    event: 'issue_comment',
+    secrets: ['GITHUB_TOKEN']
+});
+
+const label = tools.inputs.label || 'Port Request';
+
+
+async function run() {
+    try {
+        let prNumber = tools.context.payload?.pull_request?.number;
+
+        if (prNumber === undefined) {
+            console.error('PR Number was undefined');
+            tools.log.error('PR Number was undefined');
+            return;
+        }
+        tools.github.issues.addLabels({
+            ...tools.context.repo,
+            labels: [label],
+            issue_number: prNumber
+        });
+
+    } catch (ex) {
+        console.error(ex);
+        tools.log.error(ex);
+    }
+}
